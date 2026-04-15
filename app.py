53eb4f7b37f41791f730ff939368a490d48ec048
deep1863 <133334605+deep1863@users.noreply.github.com>
2026-04-15 17:08:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -343,4 +343,6 @@ if __name__ == '__main__':
     print("  Running at: http://0.0.0.0:5000")
     print("  LAN Access: http://<YOUR-IP>:5000")
     print("="*55 + "\n")
-    app.run(host='0.0.0.0', port=5000, debug=False)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
